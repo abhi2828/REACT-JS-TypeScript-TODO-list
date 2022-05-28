2e6a3ebf1f90ffa98900988dcc7c229d0573860b
abhi2828 <abhi.mishra280896@gmail.com>
2022-05-28 21:28:12 +0530

    added all changes like responsive css and toster

--- src/MyComp/Todo.tsx
+++ src/MyComp/Todo.tsx
@@ -45,7 +45,7 @@ const ToDo = () => {
       ]);
       toast.success("Todo added sucessfully", {
         position: "top-center",
-        autoClose: 5000,
+        autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: false,
         pauseOnHover: false,
